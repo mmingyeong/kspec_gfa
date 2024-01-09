0d3435d02133a0bc4866ba46e339cce7b62c6453
양민경 <girlalsrud@gmail.com>
2024-01-09 11:30:34 +0900

    .

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -20,7 +20,10 @@ author = "Mingyeong Yang"
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ["sphinx.ext.autodoc"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx_rtd_theme",
+    ]
 
 templates_path = ["_templates"]
 exclude_patterns = []
@@ -29,7 +32,17 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "sphinx_rtd_theme"
+# html_theme = "sphinx_rtd_theme"
+
+# on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+# only import and set the theme if we're building docs locally
+if not on_rtd:
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# 출처: https://cimple.tistory.com/416 [CIMPLE - Christ Is My Personal Lord & Everything:티스토리]
+
 html_static_path = ["_static"]
 html_logo = "_static/kasi.png"
 html_title = "KSPEC-GFA"
